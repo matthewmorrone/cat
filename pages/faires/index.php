a21a1b5c9cd55ac35b6e64827c25892674472d74
Matthew Morrone <matthewmorrone1@gmail.com>
2015-12-28 17:32:11 -0500

    combined plain page and styled page

--- pages/faires/index.php
+++ pages/faires/index.php
@@ -1,2 +1,3 @@
 <?
+header('Access-Control-Allow-Origin: *');  
 echo file_get_contents("http://www.cattransit.com/fares/fixed-route/");
--- pages/routes/index.php
+++ pages/routes/index.php
@@ -1,2 +1,3 @@
 <?
+header('Access-Control-Allow-Origin: *');
 echo file_get_contents("http://www.cattransit.com/routes-and-schedules/");
